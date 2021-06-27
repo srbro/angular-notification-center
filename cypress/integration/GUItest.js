2f40b1514239ee07845172ac33d2d5c14117fe88
Srba Miljkovic <srba.miljkovic@gmail.com>
2021-06-27 22:11:22 +0200

    Solved problem with grabing the text from previous page, so improved test on Landing page, added comments and improved test for deliting notification

--- cypress/integration/GUItest.js
+++ cypress/integration/GUItest.js
@@ -12,14 +12,34 @@ describe("Smoke Test Set", () => {
         cy.frameLoaded('#iframe')
     })
 
-    it("Landing page", () => {
+    it("Landing page - Mehr button, Correct title and Close button", () => {
 
-        //check if the notification post has the correct title
-        cy.iframe().find('[data-cy="titleNotification"]')
-        .should('contain', 'Nio Inc was removed from you portfolio. Das Unternehmen entspricht nicht mehr den Yova Kriterien und ist nicht mehr langer im Yova-Universum verfubar')
+        //Test "Mehr" button and check if the notification post has the correct title
+        cy.iframe().find('.infobox').then( infobox => {
 
-        //click on the "Mehr" button
-        cy.iframe().find('[data-cy="buttonMehr"]').should('contain', 'Mehr').click()
+            //Save the infobox text into var text
+            const text = infobox.text()
+
+            //Mehr button - check the text and click on it
+            cy.iframe().find('[data-cy="buttonMehr"]').should('contain', 'Mehr').click()
+            cy.frameLoaded('#iframe')
+            cy.iframe().find('.notifications').should('be.visible')
+            
+            //Find the first notification element
+            cy.iframe().find('[data-cy="title"]').first().then ( title =>{
+
+                //After entering the Notification center and finding the first notification, save it's title text into the var text2
+                const text2 = title.text()
+                cy.log('text 2 = ' + text2)
+                cy.log('text = ' + text)
+
+                //Check if Infobox text, from the landing page, contains the title text of the first notification in the Notification center page
+                cy.wrap(text).should('contain', text2)
+            })
+
+        })
+
+        //Return to the landing page
         cy.go('back')
 
         //Test the Close button
@@ -30,18 +50,12 @@ describe("Smoke Test Set", () => {
     })
 
     it("Notification center page -> First Notification -> Active Circle on/off", () => {
-
-        // // PROBLEM: can't read the object from the previous page (maybe).     Grab the notification text, click on the "Mehr" button and compare title of the first notification to the grabbed text
-        // cy.iframe().find('[data-cy="titleNotification"]').invoke('text').then( text => {
-        //     cy.iframe().find('[data-cy="buttonMehr"]').click()
-        //     expect(text).to.contain(title)
-        //     //cy.iframe().find('[data-cy="title"]').should('contain', text)
-        //     //cy.iframe().wrap(text).should('contain', title)
-        // })
-            
+        
         cy.iframe().find('[data-cy="buttonMehr"]').click()
         cy.frameLoaded('#iframe')
-        cy.iframe().find('[data-cy="notificationPost"]').first()
+
+        //Check if Active Circle is visible in the first notification and if it dissapears after the click on the notification
+        cy.iframe().find('#mat-expansion-panel-header-0').first()
             .find('[data-cy="activeCircle"]')
             .should('be.visible')
             .parent().click()
@@ -55,36 +69,35 @@ describe("Smoke Test Set", () => {
         cy.iframe().find('[data-cy="buttonMehr"]').click()
         cy.frameLoaded('#iframe')
 
+        //Check if the expanded area of the first notification is visible and click on the first notification
         cy.iframe().find('#mat-expansion-panel-header-0')
             .find('#cdk-accordion-child-0')
             .should('not.exist')
         cy.iframe().find('#mat-expansion-panel-header-0')
-            .first().click()
+            .click()
+
+        //In the first notification expanded area check the existance of Delete button and if the description ccontains the text "removed"
         cy.iframe().find('#cdk-accordion-child-0')
             .should('be.visible')
-        cy.iframe().find('[data-cy="deleteBtn"]')
-            .first()
+            .find('[data-cy="deleteBtn"]')
             .should('be.visible')
+            .parents('.accordion-content').click()
+            .find('.description > p').first()
+            .should('contain', 'removed')
+
+        //Click again on the first notification title and check if the expandable area is collapsed 
         cy.iframe().find('#mat-expansion-panel-header-0')
-            .first().click()
+            .click()
         cy.iframe().find('#cdk-accordion-child-0')
             .should('not.be.visible')
+
+        //Test the function of deleting the first notification
         cy.iframe().find('#mat-expansion-panel-header-0')
-            .first().click()
-
-        // NEED TO FIND A WAY TO COLLECT THE TEXT OF THE NODE, DELETE THE NODE AND CHECK BY COLLECTED TEXT IF IT IS NOT.EXISTING
-        // cy.iframe().find('[data-cy="title"]').first().invoke('text').then( text => {
-        //     const 
-        //     expect(text).to.equal
-        //     //cy.iframe().find('[data-cy="deleteBtn"]')
-
-        // })
+            .click()
         cy.iframe().find('[data-cy="deleteBtn"]')
             .first()
             .click()
-        
-            //.get('[data-cy="notificationPost"]').first()
-
+        cy.iframe().find('#notify-0').should('have.css', 'display').and('match', /none/)
         
     })
 
